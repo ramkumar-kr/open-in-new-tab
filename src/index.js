48e10b25e389d3b2a805b9a23aed60fddc910157
Ramkumar <ramkumar.kr94@gmail.com>
2017-06-04 15:04:08 +0530

    Version 0.0.3
    Use a toggle switch

--- src/index.js
+++ src/index.js
@@ -6,12 +6,8 @@ var Popup = function (props) {
   return (
     <div>
       <form onSubmit={onSubmit}>
-        <table><tbody>
-          <tr>
-            <td className="description">Domain</td>
-            <td className="data"><h3>{getHostname(props.url)}</h3></td>
-          </tr></tbody>
-        </table>
+        <h2>{getHostname(props.url)}</h2>
+        <hr/>
         <input type="hidden" id="tabId" value={props.tabId} />
         <input type="hidden" id="hostname" value={getHostname(props.url)} />
         <div id="buttonholder"></div>
@@ -27,11 +23,14 @@ var Button = function (props) {
       <table>
         <tbody>
           <tr>
-            <td className="description">Enabled</td>
-            <td className="data"><span className={props.checked.toString()}>{props.checked.toString()}</span></td>
+            <td>
+              <label className="switch">
+                <input id="toggle" type="checkbox" onChange={props.onClick} checked={props.checked} />
+                <div className="slider round"></div>
+              </label>
+            </td>
           </tr></tbody>
-        </table>
-      <button id="toggle" className="toggle-button" onClick={props.onClick}> Toggle </button>
+      </table>
     </div>
   )
 };
@@ -58,7 +57,7 @@ var onSubmit = function (e) {
 
 function showPopup(tab) {
   var url = new URL(tab.url);
-  if(url.protocol == "http:" || url.protocol == "https:"){
+  if (url.protocol == "http:" || url.protocol == "https:") {
     ReactDom.render(<Popup url={tab.url} tabId={tab.id} />, document.getElementById("container"));
   }
 
