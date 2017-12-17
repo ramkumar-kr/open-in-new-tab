af3d4b5edd6b04d4e40a9415b0239f9f5b302ae6
Ramkumar <ramkumar.kr94@gmail.com>
2017-12-17 14:26:13 +0530

    Version 0.0.8 beta 1
    - Do not run anchor tag links when href is #. This should fix issue #4
    - Do some special tweaks for youtube
    - Add dark and light themed icons

--- tabify.js
+++ tabify.js
@@ -1,8 +1,11 @@
-function addTarget() {
+function addTarget(hostname) {
 	var elements = document.getElementsByTagName("a");
 	for (var e of elements) {
-		e.setAttribute("target", "_blank");
-		e.setAttribute("rel", "nofollow");
+		if(e.getAttribute("href") !== "#") {
+			e.setAttribute("target", "_blank");
+			e.setAttribute("rel", "nofollow");
+			addSpecialTweaks(e, hostname);
+		}
 	}
 }
 
@@ -11,12 +14,44 @@ function filter(hostname) {
 		for(var item in items){
 		  var re = new RegExp( '^'+item.replace('*','.*')+'$' );
 			if(re.test(hostname)){
-				addTarget();
+				addTarget(hostname);
 			}
 		}
 	});
 }
 
+function addSpecialTweaks(element, hostname){
+	var regex = new RegExp('youtube');
+	if(regex.test(hostname)){
+		element.addEventListener('click', openinNew);
+		element.removeAttribute("target");
+	}
+}
+
+function clone(element) {
+	var children = element.children;
+	var clone = element.cloneNode(true);
+	for (cas of clone.childNodes) {
+		clone.removeChild(cas);
+	}
+	for (child of element.childNodes) {
+		clone.appendChild(child);
+	}
+	element.parentNode.replaceChild(clone, element);
+}
+
+function openinNew(e) {
+	e.stopPropagation();
+	e.preventDefault();
+	window.open(this.href, "_blank");
+	window.stop();
+}
+
 var url = new URL(window.location.href);
 filter(url.hostname);
 
+var observer = new MutationObserver(function () {
+	var url = new URL(window.location.href);
+	filter(url.hostname);
+});
+observer.observe(document.body, {subTree: true, attributes: true});
